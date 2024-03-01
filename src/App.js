3457c7937dea90c387d0a7eb0757dd463c98ec09
Soroush Semerkant <soroushsemer@gmail.com>
2024-03-01 13:13:23 -0500

    lab 1 complete

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import RightPanel from './components/RightPanel';
 
 function App() {
   const [csvData, setCsvData] = useState(null);
-
+  const [page, setPage] = useState(1)
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -29,9 +29,10 @@ function App() {
   }, []); 
 
   return (
-    <div className="App" style={{ backgroundColor: "#fdfffd" }}>
-       <header className="navs px-3 py-1 pt-2 w-100">
-      <h3>
+    <div className="App" style={{ backgroundColor: page === 1 ? "#fdfffd": "#fff1f9" }}>
+      <header className="navs px-3 py-1 pt-2 w-100" style={{backgroundColor: page === 1? "#317873": "#896790"}}>
+      <h3 style={{display:"flex", justifyContent:"space-between", alignItems:"center"}}>
+        <div>
             <img
               src='logo.jpg'
               height="60vh"
@@ -42,12 +43,24 @@ function App() {
             <span className="mx-3 inline text-white">
               CSE 564 Data Visualization Dashboard - Billionares Data
             </span>
+            </div>
+            <div style={{display:"flex", alignItems:"center", color:"white"}}>
+              <span className='mx-3' onClick={()=>setPage(1)} style={{cursor:"pointer", textDecoration:"none", fontSize:"24px", color: page===1 ? "turquoise":"white"}}>Lab 1</span>
+              <span className='mx-3' onClick={()=>setPage(2)} style={{cursor:"pointer", textDecoration:"none", fontSize:"24px", color: page===2 ? "pink":"white"}}>Lab 2</span>
+            </div>
           </h3>
       </header>
+      {page===1 ?
       <Row className='w-100' style={{ height: "87vh", paddingLeft: "1vw" }}>
         <LeftPanel data={csvData}/>
         <RightPanel data={csvData} />
-      </Row>
+      </Row>:<div/>
+      }
+      {page===2 ?
+      <Row className='w-100' style={{ height: "87vh", paddingLeft: "1vw" }}>
+        <div>Lab 2</div> 
+      </Row>:<div/>
+      }
     </div>
   );
 }
